4a7e4c18c4d9a0c2bf512692f2fac002da46814d
ShehzorMemon <Shezymemon0180@gmail.com>
2025-03-10 05:00:27 +0500

    completed update method

--- controller/blogController.js
+++ controller/blogController.js
@@ -110,7 +110,66 @@ const blogController = {
         // response
         return res.status(200).json({blog: blogDto});
     },
-    async update(req, res, next){},
+    async update(req, res, next){
+        // validate
+
+        const updateBlogSchema = Joi.object({
+            title: Joi.string().required(),
+            content: Joi.string().required(),
+            author: Joi.string().regex(mongodbIdPattern).required(),
+            blogId: Joi.string().regex(mongodbIdPattern).required(),
+            photo: Joi.string()
+        })
+
+        const { error } = updateBlogSchema.validate(req.body);
+
+        const {title, content, author, blogId, photo} = req.body;
+
+        // if updating photo -> delete previous photo
+        // save new photo
+
+        let blog;
+        try {
+            blog = await Blog.findOne({_id: blogId})
+        }
+        catch(error) {
+            return next(error);
+        }
+
+        if (photo) {
+            let previousPhoto = blog.photoPath;
+
+            previousPhoto = previousPhoto.split('/').at(-1);
+
+            // delete previous photo because new photo is updated
+            fs.unlinkSync(`storage/${previousPhoto}`)
+
+            // read photo as buffer
+            const buffer = Buffer.from(photo.replace(/^data:image\/(png|jpg|jpeg);base64,/, ""), 'base64')
+
+            // allot a random name
+            const imagePath = `${Date.now()}-${author}.png`;
+
+            // save locally
+            try {
+                fs.writeFileSync(`storage/${imagePath}`, buffer);
+            }
+            catch(error) {
+                next(error);
+            }
+
+            // update blog
+            await Blog.updateOne({_id: blogId}, 
+                {title, content, photoPath: `${BACKEND_SERVER_PATH}/storage/${imagePath}`}
+            );
+        }
+        else {
+            await Blog.updateOne({_id: blogId}, {title, content});
+        }
+
+        // response
+        return res.status(200).json({message: 'blog updated'});
+    },
     async delete(req, res, next){},
 }
 
