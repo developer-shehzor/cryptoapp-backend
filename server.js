6fc0c273285866c20cb76ce7bf24c1dd199a69a9
ShehzorMemon <Shezymemon0180@gmail.com>
2025-03-17 04:27:56 +0500

    added cookieParser middleware

--- server.js
+++ server.js
@@ -4,20 +4,33 @@ const { PORT } = require('./config/index');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
-const cors = require('cookie-parser');
+const cors = require('cors');
 
 const corsOptions = {
     credentials: true,
     origin: ['http://localhost:3000'],
 }
+  
 
 const app = express();
 
 app.use(cookieParser());
 
+// app.use(
+//     cors({
+//       origin: function (origin, callback) {
+//         return callback(null, true);
+//       },
+//       optionsSuccessStatus: 200,
+//       credentials: true,
+//     })
+//   );
+
+// app.use(cookieParser());
+
 app.use(cors(corsOptions));
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.use(router);
 
